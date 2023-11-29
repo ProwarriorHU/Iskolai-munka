76220e2aa262c5a2bc58ff9774dd7b7303690284
ProwarriorHU <prowarriorhun@gmail.com>
2023-11-29 14:37:26 +0100

    nov 29 szoftverteszt doga

---
+++ Szoftverteszteles/Dolgozat/2023_11_29/Library/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Library
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Feladat: Párosok metódus x-től y-ig megnézi a szám páros-e egy másik metódussal. Ezt tesztelni
+            EvenOdd(5, 10);
+        }
+        public static bool IsEven(int num)
+        {
+            if (num % 2 == 0) { return true; }
+            else { return false; }
+        }
+        public static void EvenOdd(int x, int y)
+        {
+            for (int i = x; i < y; i++)
+            {
+                if (IsEven(i)) { Console.WriteLine(i + " páros"); }
+                else { Console.WriteLine(i + " páratlan"); }
+            }
+        }
+    }
+}
---
+++ Szoftverteszteles/Dolgozat/2023_11_29/LibraryTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Library;
+
+namespace LibraryTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+        }
+    }
+}
