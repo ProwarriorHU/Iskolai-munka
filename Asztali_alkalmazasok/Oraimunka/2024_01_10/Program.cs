82c74feac64b3bf2f62bf3e62b72b37123c912b2
juhasz.mate.laszlo <juhasz.mate.laszlo@blathy.info>
2024-01-10 13:15:34 +0100

    jan 10 asztali órai

---
+++ Asztali_alkalmazasok/Oraimunka/2024_01_10/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _2024_01_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Beolvas
+            StreamReader f = File.OpenText("tavirathu13.txt");
+            while (!f.EndOfStream)
+            {
+                string line = f.ReadLine();
+
+            }
+        }
+        public class City
+        {
+            public string code;
+            public string time;
+            public string wind;
+            public int temp;
+            public City(string code, string time, string wind, int temp)
+            {
+                this.code = code;
+                this.time = time;
+                this.wind = wind;
+                this.temp = temp;
+            }
+
+        }
+    }
+}
